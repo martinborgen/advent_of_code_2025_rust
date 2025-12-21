659616ffbce2feedf71d5d085c1be5b7ba96d216
Martin Borgén <martinb.nymail@gmail.com>
2025-12-21 16:47:06 +0100

    initial idea implemented

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -29,7 +29,137 @@
 
 // Process the database file from the new inventory management system. How many of the available ingredient IDs are fresh?
 
+#[derive(Debug, Clone)]
+struct FreshRange {
+    lower: u32,
+    upper: u32,
+}
+
+#[derive(Debug, Clone)]
+struct FreshDataBase {
+    data: Vec<FreshRange>,
+}
+
+impl FreshRange {
+    fn includes(&self, other: u32) -> bool {
+        other >= self.lower && other <= self.upper
+    }
+}
+
+impl FreshDataBase {
+    pub fn is_fresh(&self, id: u32) -> bool {
+        let mut low = 0;
+        let mut mid = self.data.len() / 2;
+        let mut high = self.data.len() - 1;
+
+        while !self.data[mid].includes(id) && low + 1 != high {
+            if id < self.data[mid].lower {
+                high = mid;
+                mid = (low + high) / 2;
+            } else {
+                low = mid;
+                mid = (low + high) / 2;
+            }
+        }
+
+        self.data[mid].includes(id)
+    }
+
+    pub fn read_database(fresh_string: &str) -> FreshDataBase {
+        let mut database = Vec::new();
+
+        for line in fresh_string.lines() {
+            let (lower, upper) = line.split_once('-').expect("error splitting range");
+
+            database.push(FreshRange {
+                lower: lower.parse().expect("error parsing int"),
+                upper: upper.parse().expect("error parsing int"),
+            });
+        }
+
+        database.sort_by(|a, b| a.lower.cmp(&b.lower));
+        // database.sort();
+
+        FreshDataBase { data: database }
+    }
+}
+
+fn read_input(filename: &str) -> (String, String) {
+    let input_string = std::fs::read_to_string(filename).expect("unable to read file");
+
+    let split_idx = input_string
+        .find("\n\n")
+        .expect("Couldn't find newline split");
+
+    let (fresh_ranges, ingredients) = input_string.split_at(split_idx);
+    (fresh_ranges.to_string(), ingredients.to_string())
+}
+
+fn count_fresh(database: FreshDataBase, ids: &str) -> u32 {
+    let mut count = 0;
+
+    for id in ids.lines() {
+        if id.is_empty() {
+            continue;
+        }
+
+        let id_u32 = id.parse().expect("error converting ID to u32");
+        if database.is_fresh(id_u32) {
+            count += 1;
+        }
+    }
+
+    count
+}
 
 fn main() {
-    println!("Hello, world!");
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_import_database() {
+        let (fresh, ingredients) = read_input("data/sample_input");
+
+        assert_eq!(fresh, "3-5\n10-14\n16-20\n12-18");
+        assert_eq!(ingredients, "\n\n1\n5\n8\n11\n17\n32\n");
+
+        let database = FreshDataBase::read_database(&fresh);
+        assert_eq!(database.data[0].lower, 3);
+        assert_eq!(database.data[0].upper, 5);
+        assert_eq!(database.data[1].lower, 10);
+        assert_eq!(database.data[1].upper, 14);
+        assert_eq!(database.data[2].lower, 12);
+        assert_eq!(database.data[2].upper, 18);
+        assert_eq!(database.data[3].lower, 16);
+        assert_eq!(database.data[3].upper, 20);
+    }
+
+    #[test]
+    fn test_find_range() {
+        let (fresh, ingredients) = read_input("data/sample_input");
+
+        let database = FreshDataBase::read_database(&fresh);
+
+        assert_eq!(database.is_fresh(1), false);
+        assert_eq!(database.is_fresh(5), true);
+        assert_eq!(database.is_fresh(8), false);
+        assert_eq!(database.is_fresh(11), true);
+        assert_eq!(database.is_fresh(17), true);
+        assert_eq!(database.is_fresh(32), false);
+    }
+
+    #[test]
+    fn test_sample_input_part_1() {
+        let (fresh, ingredients) = read_input("data/sample_input");
+
+        let database = FreshDataBase::read_database(&fresh);
+
+        let fresh_count = count_fresh(database, &ingredients);
+
+        assert_eq!(fresh_count, 3);
+    }
 }
